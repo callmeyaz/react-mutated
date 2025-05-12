dd6e483dd2d0f1602a7755e6db5d11149bc2f7da
yaz <AAhmed@eimagine.com>
2025-05-12 17:04:35 -0400

    Joi

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './app/app-zod.tsx'
+import App from './app/app-joi'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
